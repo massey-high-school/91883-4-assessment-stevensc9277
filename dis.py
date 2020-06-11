967e74705f29690c37ff6c9852a9e02cff509482
Caleb Stevens <stevensc9277@masseyhigh.school.nz>
2020-06-11 22:52:46 +0000

    randomly generates numbers and added a function to check answers

--- dis.py
+++ dis.py
@@ -1,71 +1,47 @@
 # Program calculates discriminant of a quadratic equation
 # To do
+import random
 import math
 
+# Answer checking function (Must be either >, < or =)
+def ans_check():
+  response = input("Is the discriminant < 0, > 0 or = 0: ")
+  if response == ">" and dis == 0 or response == "<" and dis == 0:
+    print("Discriminant was = 0")
+    print()
+  elif response == ">" and dis < 0 or response == "=" and dis < 0:
+    print("Discriminant was < 0")
+    print()
+  elif response == "<" and dis > 0 or response == "=" and dis > 0:
+    print("Discriminant was > 0")
+    print()
+  elif response == ">" and dis > 0:
+    print("That is correct!")
+    print()
+  elif response == "<" and dis < 0:
+    print("That is correct!")
+    print()
+  elif response == "=" and dis == 0:
+    print("That is correct!")
+    print()
+  else:
+    print("That was not expected, so I won't count that. Here's an extra question: ")
 
-# Number checker
-def num_check(question, low=None, high=None):
-    # sets up error messages
-    if low is not None and high is not None:
-        error = "Please enter an integer between {} and {} " \
-                "(inclusive)".format(low, high)
-    elif low is not None and high is None:
-        error = "Please enter an integer that is more than or " \
-                "equal to {}".format(low)
-    elif low is None and high is not None:
-        error = "Please enter an integer that is less than or " \
-                "equal to {}".format(high)
-    else:
-        error = "Please enter an integer"
+total_score = 5
 
-    while True:
+num_1 = random.randint(0, 10)
+num_2 = random.randint(0, 10)
+num_3 = random.randint(0, 10)
 
-        try:
-            response = int(input(question))
-            # Checks response is not too low
-            if low is not None and response < low:
-                print(error)
-                continue
+for item in range(0, 5):
+  print("{}x² + {}x + {} = 0".format(num_1, num_2, num_3))
 
-            # Checks response is not too high
-            if high is not None and response > high:
-                print(error)
-                continue
+  dis = math.pow(num_2, 2) - 4 * (num_1 * num_3)  
+  answer = ans_check()
+  print("The discriminant was {:.0f}".format(dis))
+  print()
+  num_1 = random.randint(0, 10)
+  num_2 = random.randint(0, 10)
+  num_3 = random.randint(0, 10)
 
-            return response
-
-        except ValueError:
-            print(error)
-            continue
-
-# Get user input
-num_1 = num_check("Enter a number to assume 'a': ")
-
-num_2 = num_check("Enter another number to assume 'b': ")
-
-num_3 = num_check("Enter your last number to assume 'c': ")
-
-# Calculate discrim...
-discriminant = math.pow(num_2, 2) - (4 * num_1 * num_3)
-
-# Ask user if discriminant is greater than, less than or equal to zero
-question = input("Is the discriminant less than, greater than or equal to zero? ")
-if question == "<" and discriminant < 0:
-  print("Correct")
-
-elif question == ">" or question == "=" and discriminant < 0:
-  print("Wrong. Try again")
-
-elif question == ">" and discriminant > 0:
-  print("Correct")
-
-elif question == "<" or question == "=" and discriminant > 0:
-  print("Wrong. Try again")
-
-elif question == "=" and discriminant == 0:
-  print("Correct")
-
-elif question == "<" or question == ">" and discriminant == 0:
-  print("Wrong. Try again")
-
-print("The discriminant was {:.2f}".format(discriminant))
+print("You scored {:.2f}% ".format(total_score / 5 * 100))
