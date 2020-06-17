a1b31ac82a2dfb21fc12b690563ac586c8edba32
Caleb Stevens <stevensc9277@masseyhigh.school.nz>
2020-06-17 00:58:50 +0000

    replace 1 with "", not including last integer

--- randint.py
+++ randint.py
@@ -1,7 +1,10 @@
 import random
 
-num_1 = random.randint(0, 10)
-num_2 = random.randint(0, 10)
-num_3 = random.randint(0, 10)
-
-for item in 
+for item in range(0, 5):
+  num_1 = random.randint(0, 10)
+  num_2 = random.randint(0, 10)
+  num_3 = random.randint(0, 10)
+  
+  equation = "{}x^2 + {}x + {} = 0".format(num_1, num_2, num_3)
+  print(equation.replace("1", "", num_2))
+  
