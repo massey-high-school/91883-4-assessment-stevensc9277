f96ebc567682aea8c8c95932daf2f74833bbf975
Caleb Stevens <stevensc9277@masseyhigh.school.nz>
2020-06-17 01:07:52 +0000

    replace 1 with "" and changed last integer of equation so it does not get replaced too

--- randint.py
+++ randint.py
@@ -3,8 +3,8 @@ import random
 for item in range(0, 5):
   num_1 = random.randint(0, 10)
   num_2 = random.randint(0, 10)
-  num_3 = random.randint(0, 10)
+  num_3 = random.randint(2, 10)
   
   equation = "{}x^2 + {}x + {} = 0".format(num_1, num_2, num_3)
-  print(equation.replace("1", "", num_2))
+  print(equation.replace("1", "", 2))
   
