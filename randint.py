6bfdc45a6f64bb449f41648e5fe8278c28423070
Caleb Stevens <stevensc9277@masseyhigh.school.nz>
2020-06-17 23:41:17 +0000

    replace 1 with ""

--- randint.py
+++ randint.py
@@ -5,6 +5,5 @@ for item in range(0, 5):
   num_2 = random.randint(0, 10)
   num_3 = random.randint(2, 10)
   
-  equation = "{}x^2 + {}x + {} = 0".format(num_1, num_2, num_3)
-  print(equation.replace("1", "", 2))
-  
+  equation = ("{}x^2 + {}x + {} = 0".format(num_1, num_2, num_3))
+  print(equation.replace("1", ""))
