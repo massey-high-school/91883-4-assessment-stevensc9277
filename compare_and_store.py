6edcd0211815955a58863d32109817002e1e2424
Caleb Stevens <stevensc9277@masseyhigh.school.nz>
2020-06-24 00:53:35 +0000

    store question answered incorrectly in list then print at end

--- compare_and_store.py
+++ compare_and_store.py
@@ -3,23 +3,21 @@
 import random
 import math 
 
+# Answer checker / comparing check
 def ans_check():
   response = input("Is the discriminant < 0, > 0 or = 0: ")
   if response == ">" and dis == 0 or response == "<" and dis == 0:
     print("Discriminant was = 0")
     print()
     wrong_list.append(equation)
-    wrong_list.append(response)
   elif response == ">" and dis < 0 or response == "=" and dis < 0:
     print("Discriminant was < 0")
     print()
     wrong_list.append(equation)
-    wrong_list.append(response)
   elif response == "<" and dis > 0 or response == "=" and dis > 0:
     print("Discriminant was > 0")
     print()
     wrong_list.append(equation)
-    wrong_list.append(response)
   
   elif response == ">" and dis > 0:
     print("That is correct!")
@@ -36,11 +34,11 @@ def ans_check():
 
 incorrect = 0
 wrong_list = []
-max_question = 5
+max_question = 2
 for item in range(0, max_question):
   num_1 = random.randint(0, 9)
   num_2 = random.randint(0, 9)
-  num_3 = random.randint(2, 10)
+  num_3 = random.randint(2, 9)
   equation = "{}x^2 + {}x + {} = 0".format(num_1, num_2, num_3)
   print(equation.replace("1", ""))
   dis = math.pow(num_2, 2) - 4 * (num_1 * num_3)  
@@ -49,4 +47,7 @@ for item in range(0, max_question):
   print()
 
 print("You got these incorrect:")
-print("{}".format(wrong_list))
+
+# Print incorrect questions
+for item in wrong_list:
+  print(item)
