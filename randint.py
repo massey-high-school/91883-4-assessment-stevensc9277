77ddd67ded3572f89397f9bf0828342c9f6e7426
Caleb Stevens <stevensc9277@masseyhigh.school.nz>
2020-06-24 00:59:47 +0000

    Create random integer for a, b, c

--- randint.py
+++ randint.py
@@ -1,9 +1,10 @@
 import random
 
+# Create random integer for a, b, c
 for item in range(0, 5):
   num_1 = random.randint(0, 10)
   num_2 = random.randint(0, 10)
   num_3 = random.randint(2, 10)
-  
+# Display equation but replace 1 with "blank"
   equation = ("{}x^2 + {}x + {} = 0".format(num_1, num_2, num_3))
   print(equation.replace("1", ""))
